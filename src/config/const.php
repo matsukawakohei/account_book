e36190b927946f34e22d6aad9779dc3c9916c4aa
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-06-18 10:25:39 +0900

    [add] 明細メール登録の定数ファイルを作成

---
+++ src/config/const.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'mail_connection' => [
+    'host'             => 'imap.gmail.com',
+    'port'             => 993,
+    'subject'          => 'カード利用のお知らせ(本人ご利用分)',
+    'encrypt_tyep'     => 'AES-128-CBC',
+    'encrypt_password' => 'password',
+    'encrypt_options'  => 0
+  ]
+];
