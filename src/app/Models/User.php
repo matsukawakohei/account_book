2279a6dbb664e256de2d0d5673b2f04bd32ef167
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-06-25 11:06:01 +0900

    [fix] accountのモデルを統合し、リレーションを再構築

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,13 +44,13 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function manualAccounts()
+    public function manualAccounts(): HasMany
     {
-        return $this->hasMany('\App\Models\ManualAccount');
+        return $this->hasMany(Account::class);
     }
 
-    public function mailConnections()
+    public function mailConnections(): HasMany
     {
-        return $this->hasMany('\App\Models\MailConnection');
+        return $this->hasMany(MailConnection::class);
     }
 }
