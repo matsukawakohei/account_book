a24d46bf7b63a2b3d5557f46c8c7ff9c385db8da
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-07-02 08:12:20 +0900

    [fix] アラート表示部分をコンポーネントに変更

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -40,11 +40,7 @@
 @stop
 
 @section('content')
-    @if (session('flash_message'))
-        <div class="flash_message bg-success text-center py-3 my-0">
-            {{ session('flash_message') }}
-        </div>
-    @endif
+    <x-alert />
     <div class="row">
         <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box">
