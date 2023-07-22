9a3792ba6a72fa508724ecf29dceb159b8505a05
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-07-22 20:05:25 +0900

    [fix] 更新と削除のポリシーを追加

--- src/app/Policies/AccountPolicy.php
+++ src/app/Policies/AccountPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Account;
 use App\Models\User;
 
 class AccountPolicy
@@ -12,5 +13,15 @@ class AccountPolicy
     public function __construct()
     {
         //
+    }
+
+    public function update(User $user, Account $account): bool
+    {
+        return $user->id === $account->user_id;
+    }
+
+    public function delete(User $user, Account $account): bool
+    {
+        return $user->id === $account->user_id;
     }
 }
